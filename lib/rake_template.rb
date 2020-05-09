a1b167d0f4646841a4c56d21c8beb496f9e25cba
Toby Clemson <tobyclemson@gmail.com>
2020-05-09 01:33:37 +0100

    Add helper function for render task.

--- lib/rake_template.rb
+++ lib/rake_template.rb
@@ -2,4 +2,7 @@ require 'rake_template/tasks'
 require 'rake_template/version'
 
 module RakeTemplate
+  def self.define_render_task(opts = {}, &block)
+    RakeTemplate::Tasks::Render.define(opts, &block)
+  end
 end
--- spec/rake_template_spec.rb
+++ spec/rake_template_spec.rb
@@ -4,4 +4,30 @@ RSpec.describe RakeTemplate do
   it 'has a version number' do
     expect(RakeTemplate::VERSION).not_to be nil
   end
+
+  context 'define_render_task' do
+    context 'when instantiating RakeTemplate::Tasks::Render' do
+      it 'passes the provided block' do
+        opts = {
+            template_file_path: 'some/template/file',
+            output_file_path: 'some/output/file',
+        }
+
+        block = lambda do |t|
+          t.vars = {
+              first: "one",
+              second: "two"
+          }
+        end
+
+        expect(RakeTemplate::Tasks::Render)
+            .to(receive(:define) do |passed_opts, &passed_block|
+              expect(passed_opts).to(eq(opts))
+              expect(passed_block).to(eq(block))
+            end)
+
+        RakeTemplate.define_render_task(opts, &block)
+      end
+    end
+  end
 end
